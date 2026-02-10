c56122c7eac5f89a92fb4f02b32206d40e166843
nlyaizzati <alya@alphia.net>
2026-02-10 18:54:45 +0800

    Update localization import path in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:resayil_app/core/l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'core/config/theme/app_theme.dart';
 import 'core/router/app_router.dart';
